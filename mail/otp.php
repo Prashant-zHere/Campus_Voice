9bf1ae8e758f3f7c9fe6770e8f22974c87f93f7a
Prashant Sharma <zedd.prashant@gmail.com>
2026-01-01 21:50:13 +0530

    Update otp.php

--- mail/otp.php
+++ mail/otp.php
@@ -77,8 +77,8 @@
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'fakeacc.9867@gmail.com';                     //SMTP username
-            $mail->Password   = 'vkwnkmwqwumgiics';                               //SMTP password
+            $mail->Username   = '${{secrets.EMAIL}}';                     //SMTP username
+            $mail->Password   = '${{secrets.PASSWORD}}';                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
